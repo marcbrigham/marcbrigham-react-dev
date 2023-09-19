be8966361b2d36770707a5527281fb87b7a27028
Marc Brigham <marc.brigham@gmail.com>
2023-09-19 16:35:58 -0400

    updated content and added images

---
+++ src/data.js
@@ -0,0 +1,67 @@
+export const projects = [
+    {
+        title: "Oklahoma State University Athletics",
+        subtitle: "HTML, LESS, & Knockout.js",
+        description:"My work on okstate.com involved crafting a seamless online experience that resonates with the university's athletic community, showcasing events, news, and athlete profiles with a modern touch.",
+        image: "./OKState_devices.jpg",
+        link: "https://okstate.com/",
+    },
+    {
+      title: "Baylor University Athletics",
+      subtitle: "HTML, LESS, & Knockout.js",
+      description:
+        "My work on baylorbears.com involved crafting a seamless online experience that resonates with the university's athletic community, showcasing events, news, and athlete profiles with a modern touch.",
+      image: "./Baylor_devices.jpg",
+      link: "https://baylorbears.com/",
+    },
+    {
+      title: "Post Game",
+      subtitle: "JS (Vue), CSS (SASS), PHP",
+      description:
+        "I developed an advanced feature for FantasyPros, providing users with a post-game analysis of their fantasy football league's performance each week.",
+      image: "./post-game-all-play.png",
+      link: "https://www.fantasypros.com/nfl/myplaybook/post-game.php",
+    },
+    {
+      title: "Draft Intelligence",
+      subtitle: "JS (Vue), CSS (SASS), Java (JSP)",
+      description:
+        "I created a pre-draft tool for upcoming NFL seasons, offering users insights into potential drafting patterns, allowing them to anticipate their league mates' drafting strategies.",
+      image: "./draft-intel-league-patterns.png",
+      link: "https://pythonbootcamp.com",
+    },
+  ];
+
+  export const skills = ["Javascript", "Vue", "Angular", "LESS", "SASS", "Tailwind", "Bootstrap", "Git", "Figma", "Storybook", "Agile", "Google Analytics"];
+
+  export const testimonials = [
+    {
+        name: "Lorum",
+        quote:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-1.gif",
+        company: "google",
+    },
+    {
+        name: "Lorum",
+        quote:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-2.gif",
+        company: "google",
+    },
+    {
+        name: "Lorum",
+        subtitle: "React and Firebase",
+        quote:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-3.gif",
+        company: "google",
+    },
+    {
+        name: "Lorum",
+        quote:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-4.gif",
+        company: "google",
+    },
+  ];
