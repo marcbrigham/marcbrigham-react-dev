1c193352df83bd1174efdec9ed9ea144637c095d
Marc Brigham <marc.brigham@gmail.com>
2023-09-19 08:43:00 -0400

    add contact, about, navbar, projects, skills, and testimonials

---
+++ src/components/Testimonials.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { testimonials } from "../data";
+
+export default function Testimonials() {
+  return (
+    <section id="testimonials">
+      <div className="container px-5 py-10 mx-auto text-center">
+        <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-12">
+          Client Testimonials
+        </h1>
+        <div className="flex flex-wrap m-4">
+          
+        </div>
+      </div>
+    </section>
+  );
+}
