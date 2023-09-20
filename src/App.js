f0a90d5ee610b90e2739d81d05d3e253a45dbe87
Marc Brigham <marc.brigham@gmail.com>
2023-09-20 04:37:48 -0400

    fix react ga events

--- src/App.js
+++ src/App.js
@@ -5,11 +5,10 @@ import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 //import Testimonials from "./components/Testimonials";
-import { ReactGA } from 'react-ga';
+import ReactGA from "react-ga4";
 
 export default function App() {
   ReactGA.initialize('UA-13212828-3');
-  ReactGA.pageview('/');
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
